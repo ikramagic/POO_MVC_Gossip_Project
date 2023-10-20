a12f15079625f3b3ee9fd6aefe3266a7d43e1de6
ikramiste <ikrame.saadi@gmail.com>
2023-10-20 17:08:43 +0200

    needs a fix

--- lib/controller.rb
+++ lib/controller.rb
@@ -4,6 +4,15 @@ require_relative 'view'
 class Controller
 
     def create_gossip
+
+        params = {
+            author: @view.author
+            content: @view.content
+        }
         @gossip = Gossip.new
+        gossip = Gossip.new(:author, :content) #crée une instance de la classe Gossip
+        gossip.save #demande au model de l'enregistrer dans le CSV
+        @view = View.new 
+        @view.create_gossip #exécute la méthode create_gossip de la View afin de récupérer les infos de l'utilisateur
     end
 end
