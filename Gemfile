9da9e5cec44ce847249880588c92f94ef4927857
ikramiste <ikrame.saadi@gmail.com>
2023-10-20 14:34:51 +0200

    first commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org/'
+gem 'pry', '~> 0.14.2'
+gem 'csv'
+ruby '3.2.2'
---
+++ app.rb
@@ -0,0 +1,8 @@
+require 'bundler'
+Bundler.require
+
+require_relative 'lib/controller'
+require_relative 'lib/gossip'
+require_relative 'lib/router'
+require_relative 'lib/view'
+
