a5e3915f341943b98f3844f4f93cd2065fb6ce9a
ikramiste <ikrame.saadi@gmail.com>
2023-10-22 13:21:31 +0200

    fixed repeating issue before capturing gossip

--- lib/controller.rb
+++ lib/controller.rb
@@ -4,15 +4,17 @@ require_relative 'view'
 class Controller
 
     def create_gossip
-
-        params = {
-            author: @view.author
-            content: @view.content
-        }
-        @gossip = Gossip.new
-        gossip = Gossip.new(:author, :content) #crée une instance de la classe Gossip
-        gossip.save #demande au model de l'enregistrer dans le CSV
-        @view = View.new 
-        @view.create_gossip #exécute la méthode create_gossip de la View afin de récupérer les infos de l'utilisateur
+        @view = View.new #crée une instance de la classe View 
+        #DELETED & WHY: 
+        #@view.create_gossip- PAS BESOIN CAR DEJA EXECUTE AVEC LA CREATION DE L'INSTANCE DE CLASSE View
+        params = @view.create_gossip 
+            #indique à la méthode create_gossip de Controller où récupérer le hash params dont on aura besoin
+        @gossip = Gossip.new(params[:author], params[:content])
+            #crée une instance de la classe Gossip avec les valeurs de la hash params
+        @gossip.save 
+            #demande au model (=gossip) de l'enregistrer dans le CSV
     end
 end
+
+#à noter qu'il est aussi possible, de manière un peu moins compacte 
+#de créer une variable gossip = Gossip.new... pour créer l'instance de classe, et ensuite d'utiliser cette variable pour appliquer la méthode save
